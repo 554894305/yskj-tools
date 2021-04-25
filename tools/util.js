139b14b3d35eab6ef73a01c253ed4857548663a1
xiehuan <389563892@qq.com>
2021-04-25 18:30:23 +0800

    4-25

--- tools/util.js
+++ tools/util.js
@@ -1,59 +1,119 @@
-export function _gotos(url, str) {
-    if(!str) str = 'page'
-    if(str == 'page') {
-        uni.navigateTo({
-            url
-        })
-    }
-    if(str == 'tab') {
-        uni.switchTab({
-            url
-        })
-    }
-    if(str == 'redirect') {
-        uni.redirectTo({
-           url
-        })
+/**
+ * 数据节流
+ */
+let enterTime = 0
+export function _throttle(fn, interval) {
+    let gapTime = interval || 300
+    return function() {
+        let context = this
+        let backTime = new Date()
+        if (backTime - enterTime > gapTime) {
+            fn.call(context, arguments)
+            enterTime = backTime
+        }
     }
 }
+
 /**
- * 返回星期
+ * 数据防抖
  */
- export function _forWeek(week, str) {
-    if(!str) {
-        str = '周'
-    }
-    let arr = [{
-        key: 1,
-        label: '一'
-    },{
-        key: 2,
-        label: '二'
-    },{
-        key: 3,
-        label: '三'
-    },{
-        key: 4,
-        label: '四'
-    },{
-        key: 5,
-        label: '五'
-    },{
-        key: 6,
-        label: '六'
-    },{
-        key: 7,
-        label: '日'
-    }]
-    let result = ''
-    if(week) {
-        arr.map((item) => {
-            if(item.key === Number(week)) {
-                result = str +  item.label
+ let timer
+ export function _debounce(fn, interval) {
+     let gapTime = interval || 800
+     clearTimeout(timer)
+     timer = setTimeout(function() {
+         fn()
+     }, gapTime)
+ }
+
+ /**
+  * 截取字符串
+  */
+export function _query(paraName) {
+    let url = decodeURIComponent(document.location.toString())
+    let arrObj = url.split('?')
+    if (arrObj.length > 2) {
+        for (let x = 1; x < arrObj.length; x++) {
+            let arrPara = arrObj[x].split('&')
+            let arr
+            for (let i = 0; i < arrPara.length; i++) {
+                arr = arrPara[i].split('=')
+                if (arr != null && arr[0] == paraName) {
+                    return arr[1]
+                }
             }
-        })
-        return result
-    }else {
+        }
+        return ''
+    } else if (arrObj.length > 1) {
+        let arrPara = arrObj[1].split('&')
+        let arr
+        for (let x = 0; x < arrPara.length; x++) {
+            arr = arrPara[x].split('=')
+            if (arr != null && arr[0] == paraName) {
+                return arr[1]
+            }
+        }
+        return ''
+    } else {
         return ''
     }
 }
+
+/** 
+ * 性别映射
+ */
+export function _sex(str) {
+    if (typeof str === 'string') {
+        return str === 'female' ? '女' : str === 'male' ? '男' : '--'
+    } else if (typeof str === 'number') {
+        return str === 0 ? '女' : str === 1 ? '男' : '--'
+    }
+}
+
+/**
+ * 生成大位随机数
+ */
+export function _random(num = 9) {
+    return Math.floor(Math.random() * Math.pow(10, num-1) + Math.pow(10, num-1))
+}
+
+/**
+ * 时间格式化 
+ * format: 默认值为 'MM-DD'，返回值例如：04-25；
+ *         'MM-DD hh:mm' => 04-25 17:43
+ *         'week' => 0
+ */
+ export function _formattingTime(time, format = 'MM-DD') {
+    let date = new Date(time)
+    var YY = date.getFullYear() //获取当前年份
+    var MM = date.getMonth() + 1 //获取当前月份
+    var DD = date.getDate() //获取当前日
+    var hh = date.getHours() //获取小时
+    var mm = date.getMinutes() //获取分钟
+    var ss = date.getSeconds() //获取秒
+    var week = date.getDay() //获取星期几
+
+    MM = MM < 10 ? '0' + MM : MM
+    DD = DD < 10 ? '0' + DD : DD
+    hh = hh < 10 ? '0' + hh : hh
+    mm = mm < 10 ? '0' + mm : mm
+    ss = ss < 10 ? '0' + ss : ss
+
+    if (format === 'MM-DD hh:mm') {
+        return MM + '-' + DD + ' ' + hh + ':' + mm
+    } else if (format === 'MM-DD') {
+        return MM + '-' + DD
+    } else if (format === 'week') {
+        return week
+    }
+}
+
+/**
+ * 数组去重
+ */
+export function _unique(data) {
+    let arr = new Set(data)
+    return [...arr]
+}
+
+
